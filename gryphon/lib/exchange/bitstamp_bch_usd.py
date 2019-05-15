db4bfbef69abb853d9ff5309ea2fbbc39f3a978f
user <gareth.macleod@gmail.com>
2019-05-14 21:29:05 -0400

    Bitstamp BCHUSD now available for trading.

--- gryphon/lib/exchange/bitstamp_bch_usd.py
+++ gryphon/lib/exchange/bitstamp_bch_usd.py
@@ -27,6 +27,9 @@ class BitstampBCHUSDExchange(BitstampBTCUSDExchange):
         self.currency = u'USD'
         self.volume_currency = 'BCH'
 
+        self.price_decimal_precision = 2
+        self.volume_decimal_precision = 8
+
         self.fiat_balance_tolerance = Money('0.0001', 'USD')
         self.volume_balance_tolerance = Money('0.00000001', 'BCH')
         self.min_order_size = Money('0.001', 'BCH')
@@ -34,3 +37,11 @@ class BitstampBCHUSDExchange(BitstampBTCUSDExchange):
         if configuration:
             self.configure(configuration)
 
+        self.ticker_url = 'ticker/bchusd/'
+        self.orderbook_url = 'order_book/bchusd/'
+        self.buy_url = 'buy/bchusd/'
+        self.sell_url = 'sell/bchusd/'
+        self.open_orders_url = 'open_orders/bchusd/'
+        self.trade_status_url = 'user_transactions/bchusd/'
+        self.balance_url = 'balance/'
+        self.trade_cancel_url = 'cancel_order/'
