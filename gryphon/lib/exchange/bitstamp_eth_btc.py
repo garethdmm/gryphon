65199981a538330841866b773d29554c3dc4493e
user <gareth.macleod@gmail.com>
2019-05-15 18:40:40 -0400

    Bitstamp ethbtc is available for trading.

--- gryphon/lib/exchange/bitstamp_eth_btc.py
+++ gryphon/lib/exchange/bitstamp_eth_btc.py
@@ -27,9 +27,21 @@ class BitstampETHBTCExchange(BitstampBTCUSDExchange):
         self.currency = u'BTC'
         self.volume_currency = 'ETH'
 
+        self.price_decimal_precision = 5
+        self.volume_decimal_precision = 8
+
         self.fiat_balance_tolerance = Money('0.0001', 'BTC')
         self.volume_balance_tolerance = Money('0.00000001', 'ETH')
-        self.min_order_size = Money('0.001', 'ETH')
+        self.min_order_size = Money('0.03', 'ETH')
 
         if configuration:
             self.configure(configuration)
+
+        self.ticker_url = 'ticker/ethbtc/'
+        self.orderbook_url = 'order_book/ethbtc/'
+        self.buy_url = 'buy/ethbtc/'
+        self.sell_url = 'sell/ethbtc/'
+        self.open_orders_url = 'open_orders/ethbtc/'
+        self.trade_status_url = 'user_transactions/ethbtc/'
+        self.balance_url = 'balance/'
+        self.trade_cancel_url = 'cancel_order/'
