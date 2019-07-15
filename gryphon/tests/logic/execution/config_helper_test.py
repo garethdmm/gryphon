2302aa0fabf6a174084dcc89cf4be6df84ed4372
Gareth MacLeod <gareth.macled@gmail.com>
2019-07-15 12:43:10 -0400

    Tiny refactor in the config helper to allow us to test how we parse config filenames easily, as well as a few simple tests for said functionality.

---
+++ gryphon/tests/logic/execution/config_helper_test.py
@@ -0,0 +1,41 @@
+"""
+Just a few exercises for our configuration library.
+"""
+import pyximport; pyximport.install()
+import unittest
+import mock
+import sure
+
+from gryphon.execution.lib import config_helper
+
+
+class TestConfigHelper(unittest.TestCase):
+    def test_conf_filenames_builtin_1(self):
+        filename = config_helper.get_conf_filename_from_strategy_name(
+            'simple_market_making',
+        )
+
+        filename.should.equal('simple_market_making.conf')
+
+    def test_conf_filenames_builtin_2(self):
+        filename = config_helper.get_conf_filename_from_strategy_name(
+            'multiexchange_linear',
+        )
+
+        filename.should.equal('multiexchange_linear.conf')
+
+    def test_conf_filenames_custom_pyx_2(self):
+        filename = config_helper.get_conf_filename_from_strategy_name(
+            'active_signal_strat.pyx',
+        )
+
+        filename.should.equal('active_signal_strat.conf')
+
+    def test_conf_filenames_custom_pyx(self):
+        filename = config_helper.get_conf_filename_from_strategy_name(
+            'coinbase_gemini_arb.pyx'
+        )
+
+        filename.should.equal('coinbase_gemini_arb.conf')
+
+
