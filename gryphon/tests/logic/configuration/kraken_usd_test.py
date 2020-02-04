e51a195ce94aa0054548861ec1bebbddc6f3633d
Elliott <itali43@gmail.com>
2020-02-03 23:29:45 -0800

    changed unit test fee values to properly match fee schedules of exchanges

--- gryphon/tests/logic/configuration/kraken_usd_test.py
+++ gryphon/tests/logic/configuration/kraken_usd_test.py
@@ -13,7 +13,7 @@ class KrakenUSDConfigurationTest(BaseConfiguration, unittest.TestCase):
     def __init__(self, *args, **kwargs):
         self.exchange_class = KrakenBTCUSDExchange
 
-        self.DEFAULT_MARKET_FEE = Decimal('0.0024')
+        self.DEFAULT_MARKET_FEE = Decimal('0.0026')
         self.DEFAULT_FIAT_TOLERANCE = Money('0.0001', 'USD')
         self.DEFAULT_VOLUME_TOLERANCE = Money('0.00000001', 'BTC')
 
